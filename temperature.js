506e4a1b60a62f2f15798c5c98787dcea2f827b8
Hector Rodriguez Perez <alu0100774429@ull.edu.es>
2015-02-11 18:23:51 +0000

    js avanzado

--- temperature.js
+++ temperature.js
@@ -4,5 +4,25 @@ function calc() {
   var finalTemperature;
   var tempTemperature = original.value;
   var expression = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
+
+  var m = temp.match(regexp);
+  
+  if (m) {
+    var num = m[1];
+    var type = m[2];
+    num = parseFloat(num);
+    if (type == 'c' || type == 'C') {
+      result = (num * 9/5)+32;
+      result = result.toFixed(1)+" Farenheit"
+    }
+    else {
+      result = (num - 32)*5/9;
+      result = result.toFixed(1)+" Celsius"
+    }
+    converted.innerHTML = result;
+  }
+  else {
+    converted.innerHTML = "aaaayy gurl, dont undestend ur tempp. gimme sum thing laik 3.4 C. aaaaaaaayyyyyyyyyyy";
+  }
   
 }
