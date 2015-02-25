2f1bcf608d1ecaf031f267f93243a0bc909c037d
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2015-02-25 19:55:24 +0000

    Karma funcionando.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat');
+var karma   = require('gulp-karma');
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('default', function() {
+  gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
+});
--- tests/tests.js
+++ tests/tests.js
@@ -1,20 +1,27 @@
 var assert = chai.assert; 
 //Prueba de Farenheit a Celsius
 suite('temperature converter test suite', function() {
+    setup(function(){
+      if (typeof __html__ !== 'undefined') {
+          document.body.innerHTML = __html__['tests/karmatest.html'];
+          original = document.getElementById('original');
+          converted = document.getElementById('converted');
+      }
+    });
     test('23 c = 73.4 Farenheit', function() {
         original.value = "23 c";
         calc();
         assert.deepEqual(converted.innerHTML, "73.4 Farenheit");
     });//Prueba de Celsius a Farenheit
-    test('73.4 f = 23 Celsius', function() {
+    test('73.4 f = 23.0 Celsius', function() {
         original.value = "73.4 f";
         calc();
-        assert.deepEqual(converted.innerHTML, "23 Celsius");
+        assert.deepEqual(converted.innerHTML, "23.0 Celsius");
     });//Prueba del mensaje de error
     test('aiomxd328752 = Failure in conversion', function() {
         original.value = "aiomxd328752";
         calc();
-        assert.match(converted.innerHTML, "Failure in conversion");
+        assert.deepEqual(converted.innerHTML, "Failure in conversion");
     });
 });
 
